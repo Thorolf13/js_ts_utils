d015c3e47d7175b49ca86d5bd00b4d40b30e78ab
Thorolf13 <32905570+Thorolf13@users.noreply.github.com>
2022-08-03 14:56:46 +0200

    Create duration.ts

---
+++ dates/duration.ts
@@ -0,0 +1,44 @@
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <thorolf13@gmail.com> wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return.
+ * ----------------------------------------------------------------------------
+ * "LICENCE BEERWARE" (Révision 42):
+ * <thorolf13@gmail.com> a créé ce fichier. Tant que vous conservez cet avertissement,
+ * vous pouvez faire ce que vous voulez de ce truc. Si on se rencontre un jour et
+ * que vous pensez que ce truc vaut le coup, vous pouvez me payer une bière en
+ * retour.
+ * ----------------------------------------------------------------------------
+ */
+
+const durationRegexp = /P(?:(?<years>\d+)Y)?(?:(?<months>\d+)M)?(?:(?<weeks>\d+)W)?(?:(?<days>\d+)D)?(?:T(?:(?<hours>\d+)H)?(?:(?<minutes>\d+)M)?(?:(?<seconds>\d+)S)?)?/;
+
+export type Duration = {
+  years:number
+  months:number
+  weeks:number
+  days:number
+  hours:number
+  minutes:number
+  seconds:number
+}
+
+export function parseDuration (str:string):Duration {
+  const match = durationRegexp.exec(str);
+  if (match == null) {
+    return null;
+  }
+
+  const result: any = match.groups;
+  for (const key in result) {
+    if (result[key] !== undefined) {
+      result[key] = parseInt(result[key]);
+    } else {
+      result[key] = 0;
+    }
+  }
+
+  return result as Duration;
+}
